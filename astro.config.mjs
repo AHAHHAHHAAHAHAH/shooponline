739bd33c2e53652d12f4dbe0b1dc14d79f574596
AHAHHAHHAAHAHAH <gabrimazzaglia56@gmail.com>
2026-02-26 19:54:03 +0100

    Fix adapter cloudflare (versione 11) e rimozione sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  site: "https://tuopub.it",
   output: "server",
-  adapter: node({ mode: "standalone" }),
-  integrations: [sitemap()],
-  vite: {
-    define: {
-      // Espone le env variables al client solo quelle prefissate PUBLIC_
-    }
-  }
+  adapter: cloudflare(),
 });
